cdd83884117f103c6ec4141527b430c9b384cb67
Cor le Roux <corleroux@gmail.com>
2021-09-12 20:33:29 +0200

    removed redux completely

--- apps/sovtech-swapi/src/main.tsx
+++ apps/sovtech-swapi/src/main.tsx
@@ -3,25 +3,9 @@ import * as ReactDOM from 'react-dom';
 
 import App from './app/app';
 
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import { Provider } from 'react-redux';
-
-import { PEOPLE_FEATURE_KEY, peopleReducer } from '@sovtech-swapi/data-access';
-
-const store = configureStore({
-  reducer: { [PEOPLE_FEATURE_KEY]: peopleReducer },
-  // Additional middleware can be passed to this array
-  middleware: [...getDefaultMiddleware()],
-  devTools: process.env.NODE_ENV !== 'production',
-  // Optional Redux store enhancers
-  enhancers: [],
-});
-
 ReactDOM.render(
-  <Provider store={store}>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </Provider>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
   document.getElementById('root')
 );
